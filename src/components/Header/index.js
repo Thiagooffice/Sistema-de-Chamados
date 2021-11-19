c5d94163f83745c0604cdf60aaf3cb0e9fee0d5e
Thiago <thiagooffice2@gmail.com>
2021-11-19 19:04:27 -0300

    Criando Header e adicionando React Icons.

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import {useContext} from 'react'
+import './header.css'
+import {AuthContext} from '../../contexts/auth'
+import avatar from '../../assets/avatar.png'
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+import './header.css'
+
+import {Link} from 'react-router-dom'
+
+export default function Header(){
+
+    const {user} = useContext(AuthContext)
+
+    return(
+        <div className="sidebar">
+           <div>
+               <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto avatar"/>
+           </div>
+
+           <Link to="/dashboard">
+           <FiHome color="#fff" size={24}/>
+           Chamados
+           </Link>
+           <Link to="/dashboard">
+           <FiUser color="#fff" size={24}/>
+           Clientes
+           </Link>
+           <Link to="/dashboard">
+           <FiSettings color="#fff" size={24}/>
+           Configurações
+           </Link>
+        </div>
+    )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,15 @@
 import { useContext } from "react"
 import {AuthContext} from '../../contexts/auth'
 
+import Header from '../../components/Header'
+
 export default function Dashboard(){
 
     const { signOut }=useContext(AuthContext)
 
     return(
     <div>
+        <Header/>
         <div>Página dashboard</div>
         <button onClick={()=> signOut()}>Fazer logout</button>
 
