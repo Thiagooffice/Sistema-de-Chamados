7c9238292a84e48f4900162aa843de40cd86e08c
Thiago <thiagooffice2@gmail.com>
2021-11-24 16:20:41 -0300

    Buscando Chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,77 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './dashboard.css'
 
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi"
 import { Link } from "react-router-dom"
+import firebase from '../../services/firebaseConection'
+import {format} from 'date-fns'
+
+
+const listRef = firebase.firestore().collection("chamados").orderBy("created","desc")
 
 export default function Dashboard() {
 
-    const [chamados, setChamados]=useState([1])
+    const [chamados, setChamados]=useState([])
+    const [loading, setLoading]=useState(true)
+    const [loadingMore, setLoadingMore]=useState(false)
+    const [isEmpty, setIsEmpty]=useState(false)
+    const [lastDocs, setLastDocs]=useState()
+
+
+
+
+
+    useEffect(()=>{
+
+        loadChamados()
+
+        return ()=>{
+
+        }
+    },[])
+
+    async function loadChamados(){
+    await listRef.limit(5)
+    .get()
+    .then((snapshot)=>{
+    updateState(snapshot)
+    })
+    .catch((err)=>{
+        console.log("Deu algum erro: ", err)
+        setLoadingMore(false)
+    })
+    setLoading(false)
+    }
+
+    async function updateState(snapshot){
+    const isCollectionEmpty = snapshot.size === 0
+
+    if(!isCollectionEmpty){
+        let lista = []
+
+    snapshot.forEach((doc)=>{
+        lista.push({
+            id: doc.id,
+            assunto: doc.data().assunto,
+            cliente: doc.data().cliente,
+            clienteId: doc.data().clienteId,
+            created: doc.data().created,
+            createdFormated: format(doc.data().created.toDate(),"dd/MM/yyy"),
+            status: doc.data().status,
+            complemento: doc.data().complemento
+
+        })
+    })
+    const lastDoc=snapshot.docs[snapshot.docs.length - 1] //Pegando o ultimo documento buscado
+    setChamados(chamados=> [...chamados, ...lista])
+    setLastDocs(lastDoc)
+    }else{
+        setIsEmpty(true)
+    }
+    setLoadingMore(false)
+    }
 
     return (
         <div>
